bbb34642e457f76049e6b7943e55af94be8745a1
jakswa <jakswa@gmail.com>
2013-01-03 21:41:40 -0500

    fixing deprecation warning

--- lib/acts_as_archive.rb
+++ lib/acts_as_archive.rb
@@ -129,11 +129,11 @@ class ActsAsArchive
           klass = eval(options[:class]) rescue nil
           
           if klass
-            klass.send :set_table_name, options[:table]
+            klass.send 'table_name=', options[:table]
           else
             eval <<-EVAL
               class ::#{options[:class]} < ActiveRecord::Base
-                set_table_name "#{options[:table]}"
+                table_name = "#{options[:table]}"
               end
             EVAL
             klass = eval("::#{options[:class]}")
